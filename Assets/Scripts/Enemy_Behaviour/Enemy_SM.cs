1a983bd0c0a0918a12d5eb04698b2ee0cf52bf3a
Luis <luis_skater96@hotmail.es>
2020-01-12 19:54:24 +0100

    Tormenta

--- Assets/Scripts/Enemy_Behaviour/Enemy_SM.cs
+++ Assets/Scripts/Enemy_Behaviour/Enemy_SM.cs
@@ -7,6 +7,8 @@ public class Enemy_SM : MonoBehaviour
     Animator m_animator;
     private float m_HP = 100.0f;
     public AudioClip m_fireSound;
+    private float m_stormDamage;
+    private bool m_enemyOut;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,7 @@ public class Enemy_SM : MonoBehaviour
 
     private void Update()
     {
+        stormDamage();
         if (m_HP<=0){
             Destroy(gameObject);
         }
@@ -26,5 +29,21 @@ public class Enemy_SM : MonoBehaviour
     public void ApplyDamage(float damage)
     {
         m_HP -= damage/2;
+    }
+    public void enemyOutStorm (float damage)
+    {
+        m_stormDamage = damage;
+        m_enemyOut = true;
+    }
+    public void enemyInStorm()
+    {
+        m_enemyOut = false;
+    }
+    private void stormDamage()
+    {
+        if (m_enemyOut)
+        {
+            m_HP -= m_stormDamage * Time.deltaTime;
+        }
     }
 }
---
+++ Assets/Scripts/Storm.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Storm : MonoBehaviour
+{
+    Transform m_sphereTransform;
+    public float m_stormDamage = 1.0f;
+    public float m_sphereDecrement = 1.0f;
+    public float m_timerValue = 20.0f;
+    private float m_timer;
+    private float m_activeTimer = 30.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_sphereTransform = GetComponent<Transform>();
+        m_timer = m_timerValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StormBehaviour();
+    }
+    void StormBehaviour()
+    {
+        Debug.Log(m_timer);
+        if (m_timer <= 0)
+        {
+
+            if (m_activeTimer >= 0)
+            {
+                m_activeTimer -= 1 * Time.deltaTime;
+                m_sphereTransform.localScale = new Vector3(m_sphereTransform.localScale.x - m_sphereDecrement * Time.deltaTime, m_sphereTransform.localScale.y - m_sphereDecrement * Time.deltaTime, m_sphereTransform.localScale.z - m_sphereDecrement * Time.deltaTime);
+            }
+            else
+            {
+                m_sphereDecrement += 3;
+                m_stormDamage += 5;
+                m_timer = m_timerValue;
+            }
+        }
+        else
+        {
+            m_timer -= 1 * Time.deltaTime;
+            m_activeTimer = 30.0f;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Enemy_SM enemyScript = other.gameObject.GetComponent<Enemy_SM>();
+            Debug.Log("Enemigo fuera del circulo");
+            if (enemyScript != null)
+            {
+                enemyScript.enemyOutStorm(m_stormDamage);
+            }
+        }
+        if (other.tag == "Player")
+        {
+            Player playerScript = other.gameObject.GetComponent<Player>();
+            Debug.Log("Jugador fuera del circulo");
+            if (playerScript != null)
+            {
+                playerScript.playerOutStorm(m_stormDamage);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Enemy_SM enemyScript = other.gameObject.GetComponent<Enemy_SM>();
+            Debug.Log("Enemigo fuera del circulo");
+            if (enemyScript != null)
+            {
+                enemyScript.enemyInStorm();
+            }
+        }
+        if (other.tag == "Player")
+        {
+            Player playerScript = other.gameObject.GetComponent<Player>();
+            Debug.Log("Jugador fuera del circulo");
+            if (playerScript != null)
+            {
+                playerScript.playerInStorm();
+            }
+        }
+    }
+}
